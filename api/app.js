7af442bafa8e20e698a6576d405e98ad3319291f
MCozhusheck <mackozuszek@gmail.com>
2020-02-15 14:49:12 +0100

    Basic graphql

--- api/app.js
+++ api/app.js
@@ -1,8 +1,33 @@
 const express = require('express');
-const app = express();
+const { ApolloServer, gql } = require('apollo-server-express');
 const port = process.env.PORT || 3000;
 require('./db/db');
 
-app.get('/', (req, res) => res.send('Hello World!'));
+const app = express();
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const schema = gql`
+  type Query {
+    me: User
+  }
+  type User {
+    username: String!
+  }
+`;
+const resolvers = {
+  Query: {
+    me: () => {
+      return {
+        username: 'Robin Wieruch'
+      };
+    }
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers
+});
+server.applyMiddleware({ app, path: '/graphql' });
+app.listen({ port }, () => {
+  console.log(`Apollo Server on http://localhost:${port}/graphql`);
+});
